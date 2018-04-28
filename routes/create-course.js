adc28d0538a148fd879f4ca298a69e63a9e3cf98
Tyler Lindberg <tlindberg@ucla.edu>
2018-04-27 17:55:19 -0700

    Integrate create-course with auth code

--- routes/create-course.js
+++ routes/create-course.js
@@ -1,8 +1,41 @@
-var express = require('express');
-var router =  express.Router();
+const express = require('express');
+const router =  express.Router();
+const db = require('../db.js');
 
 router.get('/', function(req, res, next) {
     res.render('create-course');
+});
+
+router.post('/', function(req, res, next) {
+    // Validate and sanitize data here
+
+    // Test instructor until auth code is up
+    let instructor_id = res.locals.user._id;
+
+    let course_data = {
+        name: req.body.course_name,
+        desc: req.body.course_desc,
+        assignments: [],
+        students: [],
+        instructors: [instructor_id], // Change this to include current user
+        main_instructor: instructor_id,
+        visible: req.body.course_visible ? true : false
+    };
+
+    let course = new db.Course(course_data);
+    course.save(err => {
+        if(err) {
+            console.error(err);
+            res.render('create-course', {
+                error: 'Unable to create course. Database Error.'
+            });
+        }
+        else {
+            res.render('create-course', {
+                course: course
+            });
+        }
+    });
 });
 
 module.exports = router;
--- routes/edit-course.js
+++ routes/edit-course.js
@@ -2,7 +2,32 @@ var express = require('express');
 var router =  express.Router();
 
 router.get('/', function(req, res, next) {
-    res.render('edit-course');
+    // Get current course data
+    var course = {
+        name: 'Placeholder Name',
+        desc: 'Placeholder Description',
+        visible: true
+    };
+
+    res.render('edit-course', {
+        course: course
+    });
+});
+
+router.post('/', function(req, res, next) {
+    // Verify form data is valid and update database
+
+    var course = {};
+    var success = true;
+
+    if (success) {
+        res.redirect('courses');
+    } else {
+        res.render('edit-course', {
+            course: course,
+            errormessage: "Error modifying course"
+        });
+    }
 });
 
 module.exports = router;
