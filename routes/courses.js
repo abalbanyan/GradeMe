961b45912bfb48258359aba5277c7723ed2acbb5
Iza <izanagi@g.ucla.edu>
2018-04-29 20:30:44 -0700

    basic course list ui

--- routes/courses.js
+++ routes/courses.js
@@ -1,8 +1,18 @@
 var express = require('express');
 var router =  express.Router();
 
+var mockData = [
+    {courseName: "Operating Systems",
+     courseNum: 111},
+     {courseName: "Computer networks",
+     courseNum: 118}
+];
+
 router.get('/', function(req, res, next) {
-    res.render('courses');
+    // todo: figure out how to add middleware to all that passes data to all pages
+    // TODO: When adding authentication function, add a thing to pass in usertype.
+    // res.locals.usertype = "instructor";  // TEMPORARY
+    res.render('courses', {courselist: mockData});  // TODO: put real data here
 });
 
 module.exports = router;
