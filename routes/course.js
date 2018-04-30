1d691e6695a42e93838e3b28a114ede63e20104c
Iza <izanagi@g.ucla.edu>
2018-04-29 22:33:32 -0700

    added course page, formatting
    still using mock data, need to change that later

--- routes/course.js
+++ routes/course.js
@@ -1,8 +1,16 @@
 var express = require('express');
 var router =  express.Router();
 
+var mockData = [
+    {assignName: "Stack Breaking!",
+     dueDate: new Date()},
+     {assignName: "Build another server!! HAHA",
+     dueDate: new Date()}
+];
+
 router.get('/', function(req, res, next) {
-    res.render('course');
+    // TODO: replace mock data.
+    res.render('course', {coursename: "tempCourseName", assignments: mockData } );
 });
 
 module.exports = router;
