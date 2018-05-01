fad28887e1305fc4fef794247e45494ab0499864
Abdullah Albanyan <abalbanyan@hotmail.com>
2018-04-30 20:52:17 -0700

    Some skeleton auth code.

--- auth.js
+++ auth.js
@@ -4,3 +4,28 @@
  * 
  * TODO
  */
+let jwt = require('jsonwebtoken');
+let bcrypt = require('bcryptjs');
+let secret = 'super-secret';
+
+let mongoose = require('mongoose');
+
+
+function isAuthenticated() {
+    return false;    
+}
+
+function authenticateUser(email, password) {
+    return false;
+}
+
+function registerUser(email, password) {
+    // User.create({
+    //     name: req.body.name,
+
+    // })
+    
+    // let token = jwt.sign({
+    //     id: 
+    // });
+}
--- routes/login.js
+++ routes/login.js
@@ -1,8 +1,18 @@
 var express = require('express');
 var router =  express.Router();
 
-router.get('/', function(req, res, next) {
+router.use('/', function(req, res, next) {
     res.render('login');
+});
+
+router.post('/', function(req, res, next) {
+    let errors = [];
+    if (req.query.email && req.query.password) {
+        // TODO: Insert DB query here.
+
+    } else {
+        res.render('login', { error : 'missing-username-or-password' });
+    }
 });
 
 module.exports = router;
