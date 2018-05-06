d8d065656280e572fe32735a4a61a9e09b92f9c7
Tyler Lindberg <tlindberg@ucla.edu>
2018-05-05 23:34:24 -0700

    Instructor validation for create-course

--- routes/create-course.js
+++ routes/create-course.js
@@ -3,13 +3,19 @@ const router =  express.Router();
 const db = require('../db.js');
 
 router.get('/', function(req, res, next) {
-    res.render('create-course');
+    let instructor = res.locals.user.instructor;
+
+    if(instructor) {
+        res.render('create-course');
+    } else {
+        res.redirect('error');
+    }
 });
 
 router.post('/', function(req, res, next) {
     // Validate and sanitize data here
 
-    // Test instructor until auth code is up
+    let instructor = res.locals.user.instructor;
     let instructor_id = res.locals.user._id;
 
     let course_data = {
@@ -22,20 +28,24 @@ router.post('/', function(req, res, next) {
         visible: req.body.course_visible ? true : false
     };
 
-    let course = new db.Course(course_data);
-    course.save(err => {
-        if(err) {
-            console.error(err);
-            res.render('create-course', {
-                error: 'Unable to create course. Database Error.'
-            });
-        }
-        else {
-            res.render('create-course', {
-                course: course
-            });
-        }
-    });
+    if(instructor) {
+        let course = new db.Course(course_data);
+        course.save(err => {
+            if(err) {
+                console.error(err);
+                res.render('create-course', {
+                    error: 'Unable to create course. Database Error.'
+                });
+            }
+            else {
+                res.render('create-course', {
+                    course: course
+                });
+            }
+        });
+    } else {
+        res.redirect('error');
+    }
 });
 
 module.exports = router;
