1b3854dc74512f6d67a14cd7e6d9d5e1a5965315
Abdullah Albanyan <abalbanyan@hotmail.com>
2018-05-05 18:15:37 -0700

    Added '/create-account' link to login page.

--- routes/courses.js
+++ routes/courses.js
@@ -4,10 +4,8 @@ let getUserID = require('../auth.js').getUserID;
 let db = require('../db.js');
 
 router.get('/', async function(req, res, next) {
-    let userid = await getUserID(req);
-    let user = await db.utils.getUser(userid);
-    let instructor = user.instructor;
-    let courses = await db.utils.getCourses(userid, instructor);
+    let instructor = res.locals.user.instructor;
+    let courses = await db.utils.getCourses(res.locals.user._id, instructor);
     
     res.render('courses', {
         instructor: instructor,
