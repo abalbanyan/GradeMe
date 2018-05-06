ec7ef0568c9b69051b3bcd6d77f5eeae074d06c1
Iza <izanagi@g.ucla.edu>
2018-05-06 01:10:49 -0700

    assignment page
    added drag/drop for file submission
    specs/description
    link to edit assignment page
    advanced dropdown for instructors (to upload makefiles, containers etc)
    still need to do the rest (file uploading, test cases, etc), i placed TODOs

--- routes/assignment.js
+++ routes/assignment.js
@@ -1,8 +1,19 @@
 var express = require('express');
 var router =  express.Router();
+let db = require('../db.js');
 
-router.get('/', function(req, res, next) {
-    res.render('assignment');
+router.get('/', async function(req, res, next) {
+    let instructor = res.locals.user.instructor;    
+    let assignid = req.query.assignid;
+    let assignment = await db.Assignment.findById(assignid).exec();
+    res.render('assignment', {
+        assignment: assignment, 
+        instructor: instructor
+    });
+});
+
+router.post('/', function(req, res, next) {
+    // TODO: handle file upload.
 });
 
 module.exports = router;
