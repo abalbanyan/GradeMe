e4f2da335fe66d63ebf23072c29b598ee3ec93de
Abdullah Albanyan <abalbanyan@hotmail.com>
2018-05-06 13:04:17 -0700

    Some bug fixes. New assignments are now added to course.

--- routes/create-assignment.js
+++ routes/create-assignment.js
@@ -2,39 +2,55 @@ var express = require('express');
 var router =  express.Router();
 var db = require('../db.js');
 
-router.get('/', function(req, res, next) {
+router.get('/', async function(req, res, next) {
     // TODO: Test validation more.
-    if (res.locals.user.instructor && db.utils.isCourseInstructor(req.query.courseid, res.locals.user._id)) {
-        res.render('create-assignment');
+    let isCourseInstructor = await db.utils.belongsToCourse(req.query.courseid, res.locals.user._id, true);
+    if (res.locals.user.instructor && isCourseInstructor) {
+        res.render('create-assignment', {courseid : req.query.courseid});
     } else {
         // Doesn't quite work due to format of error ejs.
         res.render('error',JSON.parse('{ "message" : "You do not have permission to view this page." }'));
     }
 });
 
-router.post('/', function(req, res, next) {
+router.post('/', async function(req, res, next) {
+    // Verify that this user is allowed to make this request.
+    let isCourseInstructor = await db.utils.belongsToCourse(req.query.courseid, res.locals.user._id, true);
+    if (!res.locals.user.instructor || !isCourseInstructor) {
+        return res.status(500);
+    }
+
     // TODO: Validate input.
     let name = req.body.name;
     let duedate = req.body.dueDate;
     let pointvalue = req.body.pointValue;
-    let description = req.body.description;
+    let desc = req.body.desc;
     let specFile = req.body.specFile;
     let realtimegrading = (req.body.RTGRadios === 'yes');
     let testcasesvisible = (req.body.testCasesViewableRadios === 'yes');
 
-    db.Assignment.create({
+    let newassignment = new db.Assignment({
         name: name,
-        desc: description,
+        desc: desc,
         grading: {
             due_date: duedate,
             total: pointvalue
         }
-    }, (err) => {
-        if (err) {
+    });
+    newassignment.save(async (err) => {
+        if (err) return res.status(500);
+    
+        // Must save new assignment to course.
+        try {
+            await db.Course.findByIdAndUpdate(req.query.courseid,
+                    {$addToSet: {assignments: newassignment._id} }).exec();
+        } catch (err) {
+            // Failure. Try to roll back previous assignment (but not a big deal if it doesn't delete).
+            console.error(err);
+            await db.Course.removeById(newassignment._id).exec();
             return res.status(500);
         }
-        // TODO: redirect to newly created assignment page
-        res.redirect('/courses');
+        res.redirect('/assignment?assignid=' + newassignment._id);
     });
 });
 
