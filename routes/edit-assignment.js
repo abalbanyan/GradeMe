48156623a39e2a7f9db0d2e04908a0bfcc9dd94e
Iza <izanagi@g.ucla.edu>
2018-05-09 20:43:23 -0700

    *sweats* edit-assignment UI done
    no backend yet
    took FOREVER to figure out filename display but i got it

--- routes/edit-assignment.js
+++ routes/edit-assignment.js
@@ -1,8 +1,23 @@
 var express = require('express');
 var router =  express.Router();
+let db = require('../db.js');
 
-router.get('/', function(req, res, next) {
-    res.render('edit-assignment');
+router.get('/', async function(req, res, next) {
+    let userid = res.locals.user._id;
+    let instructor = res.locals.user.instructor;
+    let assignid = req.query.assignid;
+    let assignment = await db.Assignment.findById(assignid).exec();
+
+    // Ensure this user belongs to the course.
+    let course = await db.Course.findOne({assignments : assignid});
+    if (!(await db.utils.belongsToCourse(course._id, userid, instructor, res.locals.user.admin))) {
+        return res.render('error', {message: "You do not belong to this course."});
+    }
+
+    res.render('edit-assignment', {
+        assignment: assignment, 
+        instructor: instructor,
+    });
 });
 
 module.exports = router;
