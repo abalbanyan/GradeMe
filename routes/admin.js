4008df099aa8e426c4f499c2dd4c0c82a98c9d1d
chris <chrszhu@yahoo.com>
2018-05-12 20:29:11 -0700

    fixed requested changes

--- routes/admin.js
+++ routes/admin.js
@@ -1,9 +1,15 @@
 var express = require('express');
 var router =  express.Router();
+let db = require('../db.js');
 
-router.get('/', function(req, res, next) {
+
+router.get('/', async function(req, res, next) {
+
     if(res.locals.user.admin === true) {
-        res.render('admin');
+        let users = await db.User.find().exec();
+        res.render('admin', {
+          myUsers: users
+      });
     } else {
         res.status(403);
         res.render('error', {message: "You do not have access to this page."});
