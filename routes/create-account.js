b7bc1f82eaf88daf0921d985e64d89049627dad6
Iza <izanagi@g.ucla.edu>
2018-05-23 18:21:30 -0700

    added email-validation, fixed missing > in create-account.ejs
    all fields except password and codes are repopulated after email validation error
    
    repopulation of codes is probs a nice to have and can be looked at later if there's time.

--- routes/create-account.js
+++ routes/create-account.js
@@ -2,34 +2,51 @@ let express = require('express');
 let router =  express.Router();
 let db = require('../db.js');
 let auth = require('../auth.js');
+let emailValidator = require('email-validator');
 
 router.get('/', function(req, res, next) {
-    res.render('create-account');
+    res.render('create-account', {
+        email: '',
+        firstname: '',
+        lastname: '',
+        userradios: '',
+        uid: ''
+    });
 });
 
 router.post('/', async function(req, res, next) {
-    let email = req.body.email;  // Bootstrap form already validates FORM of email.
+    let email = req.body.email;
     let password = req.body.password;
     let firstname = req.body.firstname;
     let lastname = req.body.lastname;
     let instructor = (req.body.userradios === 'instructor');
     let uid = req.body.uid;
-
+    
+    if (!emailValidator.validate(email)) {
+        return res.render('create-account', {
+            errormessage: 'Please enter a valid email address.',
+            email: email,
+            firstname: firstname,
+            lastname: lastname,
+            userradios: req.body.userradios,
+            uid: uid
+        });
+    }
     // Package data of new user to pass to createTempUser.
     let newUser = {
-            email: email,
-            password: password,
-            name: {first: firstname, last: lastname},
-            instructor: instructor,
-            uid: parseInt(uid)
-        };
+        email: email,
+        password: password,
+        name: {first: firstname, last: lastname},
+        instructor: instructor,
+        uid: parseInt(uid)
+    };
     
     if (req.body.codes) {
         if (!(req.body.codes instanceof Array)) {
             // Only one code was submitted. Convert to an array.
             req.body.codes = [req.body.codes];
         }
-    } 
+    }
         
     // Create temp user pre-verification.
     db.EmailVerification.createTempUser(newUser, req.body.codes, function(err, existingPersistentUser, newTempUser) {
