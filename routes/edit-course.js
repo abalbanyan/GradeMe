94366579f86924f1ef683065d2823b325393dd85
Tyler Lindberg <tlindberg@ucla.edu>
2018-05-26 12:37:56 -0700

    More route authentication tests, GET and POST

--- routes/edit-course.js
+++ routes/edit-course.js
@@ -8,10 +8,16 @@ router.get('/', async function(req, res, next) {
     const userid = res.locals.user._id;
     const admin = res.locals.user.admin;
 
-    if(courseid && instructor && await db.utils.belongsToCourse(courseid, userid, instructor, admin)) {
+    if(!courseid) {
+        res.status(404);
+        return res.render('error', {message: "No course ID provided."});
+    }
+
+    if((instructor || admin) && await db.utils.belongsToCourse(courseid, userid, instructor, admin)) {
         let course = await db.Course.findById(courseid);
         res.render('edit-course', {course: course});
     } else {
+        res.status(403);
         res.render('error', {message: "You do not have access to this course."});
     }
 });
@@ -21,6 +27,7 @@ router.post('/', async function(req, res, next) {
     // Verify and sanitize form data here
     const courseid = req.query.courseid;
     const instructor = res.locals.user.instructor;
+    const admin = res.locals.user.admin;
     const userid = res.locals.user._id;
     const courseupdate = {
         name: req.body.course_name,
@@ -28,22 +35,24 @@ router.post('/', async function(req, res, next) {
         visible: req.body.course_visible ? true : false
     };
 
-    if(courseid && instructor && await db.utils.isCourseInstructor(courseid, userid)) {
+    if(!courseid) {
+        res.status(404);
+        return res.render('error', {message: "No course ID provided."});
+    }
+
+    if((instructor || admin) && await db.utils.belongsToCourse(courseid, userid, instructor, admin)) {
         db.Course.findByIdAndUpdate(courseid, courseupdate, (err, course) => {
             if(err) {
                 console.log(err);
-                res.redirect('error');
-                // res.render('edit-course', {
-                //     course: course,
-                //     error: "Error modifying course."
-                // });
+                res.status(500);
+                res.render('error', {message: "Error finding course."});
             } else {
                 res.redirect('courses');
             }
         })
     } else {
-        // TODO: error message about no course id being provided
-        res.redirect('error');
+        res.status(403);
+        res.render('error', {message: "You do not have access to this course."});
     }
 });
 
@@ -58,7 +67,6 @@ router.delete('/', async function(req, res, next) {
                 console.log(err);
                 res.sendStatus(404);
             } else {
-                // TODO: This redirect call currently does not work in a delete call
                 res.sendStatus(200);
             }
         });
