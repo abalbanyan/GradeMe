b3041a00e4a8672c0d2c715b9309bdd07a1b5e4b
Rodrigo Valle <rdan.valle@gmail.com>
2018-06-02 16:19:46 -0700

    Fix testcase output parsing
    Comments are now parsed correctly.

--- autograder/test/env/test.sh
+++ autograder/test/env/test.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-echo "[1] (pass) Initialize payroll"
+echo "[1] (pass) Initialize payroll # Great job!"
 echo "[1] (pass) Initialize employee"
 echo "[5] (pass) Add employee to payroll"
 echo "[5] (fail) Find employee on payroll # Returned wrong employee."
