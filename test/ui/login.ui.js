9977491947fcdb553b08230162b86936f60aaa42
Tyler Lindberg <tlindberg@ucla.edu>
2018-06-06 21:59:31 -0700

    Wait for selector on login form load

--- test/ui/login.ui.js
+++ test/ui/login.ui.js
@@ -3,17 +3,17 @@ casper.options.viewportSize = { width: 1920, height: 1080 };
 
 casper.test.begin('Able to login as admin', 4, function suite(test) {
     casper.start('http://localhost:3200', function() {
-        test.assertExists('form[action="/login"]', 'login form is found');
-        this.fill('form[action="/login"]', {
-            email: 'admin@grademe.edu',
-            password: 'admin'
-        }, true);
+        this.waitForSelector('form[action="/login"]', function() {
+            test.assertExists('form[action="/login"]', 'login form is found');
+            this.fill('form[action="/login"]', {
+                email: 'admin@grademe.edu',
+                password: 'admin'
+            }, true);
+        });
     });
 
     casper.waitForUrl(/courses/, function() {
         test.assertUrlMatch(/courses/, "redirected to courses page after logging in as admin");
-
-        casper.capture('screenshots/login/1.png');
 
         // Note: buttons need to be visible in order to for click to work
         this.waitUntilVisible('a[href="/login?logout=1"]', function() {
